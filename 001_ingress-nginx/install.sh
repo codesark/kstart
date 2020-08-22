560abeeb91315fa8c3aa3284e258b8dbbe350d85
Savinay Kumar <iamsavinay@gmail.com>
2020-08-22 16:13:40 +0530

    Update install.sh
    updated the command to use newest version of ingress-nginx

--- 001_ingress-nginx/install.sh
+++ 001_ingress-nginx/install.sh
@@ -4,4 +4,4 @@
 kubectl create namespace nginx
 
 # Helm install ingress-nginx in 'ingress-nginx' namespace
-helm install ingress-nginx stable/nginx-ingress --set rbac.create=true -n nginx
+helm install ingress-nginx ingress-nginx/ingress-nginx --set rbac.create=true -n nginx
