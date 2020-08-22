c9a09630655ec36a89b68c62d37d7140539e91db
Savinay Kumar <iamsavinay@gmail.com>
2020-08-22 16:26:57 +0530

    Update install.sh
    updated cert-manager installation method

--- 002_cert-manager/install.sh
+++ 002_cert-manager/install.sh
@@ -3,13 +3,17 @@
 # Kubernetes 1.15+
 
 # Install the CustomResourceDefinition resources separately.
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.16.1/cert-manager.crds.yaml
-kubectl create namespace cert-manager
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.16.1/cert-manager.yaml
 
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
+# ---- its broken duh... too much changes
 
-helm install \
-  cert-manager jetstack/cert-manager \
-  --namespace cert-manager \
-  --version v0.14.2
+
+# kubectl create namespace cert-manager
+
+# helm repo add jetstack https://charts.jetstack.io
+# helm repo update
+
+# helm install \
+#   cert-manager jetstack/cert-manager \
+#   --namespace cert-manager \
+#   --version v0.14.2
